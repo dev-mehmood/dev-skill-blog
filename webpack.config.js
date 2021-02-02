d6e3c357a54cccdb1747ea928a7cdef1e5a308b9
dev-mehmood <dev.mehmood@gmail.com>
2021-02-03 00:54:19 +0500

    dev-skill-blogs first commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-react");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "dev-skill",
+    projectName: "blogs",
+    webpackConfigEnv,
+    argv,
+  });
+
+  return merge(defaultConfig, {
+    externals: [
+      "react",
+      "react-dom",
+      "single-spa-react",
+      "react-router-dom",
+      /^@dev-box\/.*/,
+      /^@dev-skill\/.*/,
+    ],
+    // modify the webpack config however you'd like to by adding to this object
+  });
+};
